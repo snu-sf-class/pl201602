5dfff2bd2274f92e8f70be463de2ed1653bdcdc5
Juneyoung Lee <aqjune@gmail.com>
2016-10-20 15:58:30 +0900

    Update P04.v

--- assignments/04/P04.v
+++ assignments/04/P04.v
@@ -1,7 +1,6 @@
 Require Export P03.
 
 
-
 (** Multiplication: *)
 
 Definition c_mult (n m : c_nat) : c_nat :=
@@ -12,6 +11,8 @@ Proof. exact FILL_IN_HERE. Qed.
 
 Example c_mult_2 : c_mult c_zero (c_plus c_three c_three) = c_zero.
 Proof. exact FILL_IN_HERE. Qed.
+
+(** Oct. 20 : You have to copy definition of c_plus here from P03.v. **)
 
 Example c_mult_3 : c_mult c_two c_three = c_plus c_three c_three.
 Proof. exact FILL_IN_HERE. Qed.
